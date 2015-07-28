f095ccfb0b09b259766bbcf0c364225bd2a4ce31
songzhongli <songzhongli@tuniu.com>
2015-07-28 20:31:30 +0800

    一个页面支持多个

--- dist/js/structcontent.js
+++ dist/js/structcontent.js
@@ -2,98 +2,51 @@
  * 结构数据
  * Created by songzhongli on 2015/7/18.
  */
-var structData = [
-    {
-        "title": "天堂斐济—我在斐济发现幸福",
-        "type": "advance",
-        "content": [
-            {
-                "txt": "斐济，我几年前都没听过的地方，位于南太平洋犹如仙境的小小群岛国度，极其丰富的海底景观，集世外桃源之美的瑰丽资源，上帝孕育这个缤纷的世界，还原生命中璀璨的颜色。在斐济，阳光、海浪、沙滩、珊瑚礁、茅草屋、椰林棕榈树、斐济人民是永远的画布，潜水冲浪、海钓、高尔夫是画布上动人的线条和色彩。斐济离你并不远，看再多的图片攻略都感受不到，迈开步伐打开心扉，拥抱斐济，慢慢体味，你会感受更多，收获更多。It’s Fiji time!",
-                "img": [
-                    "upload/55b3274e98c49.jpg",
-                    "upload/55b32751192d1.jpg"
-                ]
-            },
-            {
-                "txt": "备注：\n1、斐济很少涉及到小费，和当地人玩的时候，送点小礼物，小食品，他们已经超级开心了。\n2、以上行前准备能让你拥有更好的斐济之旅，避免到当地购买，因为当地没有大型工厂，产品基本都是进口，消费较高，在换成人民币就更高了。\n3、 斐济几乎所有东西都要提前预定，特别是酒店和车，出发前请在确认好。",
-                "img": [
-                    "upload/55b3275c5d5d1.jpg",
-                    "upload/55b3275e1ec07.jpg",
-                    "upload/55b3276027e2b.jpg"
-                ]
-            },
-            {
-                "txt": "驾驶注意事项：1、右边驾驶，上路前先熟悉一下 2、最高限速80，经过村庄20，警察会不定点人工测速 3、经常有小孩、牛马狗动物冲出来4、不要夜间开车 最终建议南迪包的士前往苏瓦（150FJD，不要打表哦，打表更贵）有司机兼导游也可随意观光，因为在陌生的地方交通规则毕竟不那么熟悉，避免很多交通上不必要麻烦。\n1、 抵达:斐济时间AM7:05（与中国有4小时时差）,这一刻你一定要打开遮光板提前享受这南太平洋岛国伊甸园风光，在降落过程中会经过好多小岛(有幸看到爸爸去哪拍摄取景之一心形岛哦)，像珍珠般美丽把你美醉，一夜的疲惫感全然消失。Come on！it’s Fiji time!",
-                "img": [
-                    "upload/55b3277484fff.jpg",
-                    "upload/55b32776bd7a5.jpg",
-                    "upload/55b32778a7b9d.jpg"
-                ]
-            }
-        ]
-    },
-    {
-        "title": "南迪",
-        "type": "advance",
-        "content": [
-            {
-                "txt": "1、 入境：BULA！到达南迪机场后就会第一道风景线—热情的斐济人献上当地歌曲，可以自愿投掷小费。然后排队入境，准备资料有护照、飞机上填写好的入境卡、机票行程单。海关一般会问几个简单的问题：如第一次来斐济吗，来斐济是旅游吗，停留时间，是否有朋友，在斐济的地址等等。So easy！立马你的护照上就会盖上斐济签证，停留时间4个月哦。",
-                "img": [
-                    "upload/55b3279d050fe.jpg",
-                    "upload/55b3279eb4cfd.jpg"
-                ]
-            },
-            {
-                "txt": "",
-                "img": [
-                    "upload/55b327aa5a9f5.jpg",
-                    "upload/55b327ac17400.jpg",
-                    "upload/55b327adec7f1.jpg"
-                ]
-            },
-            {
-                "txt": "最后一关：拿行李过安检，我运气好不用开箱检查，直接走过，哈哈。如果被检查行李就认真配合就好了，要fiji time哦，斐济人做事很慢很慢，从这里开始你就要fiji time！\n备注：斐济见面都说BULA，“你好“的意思\n入境时如有任何中文需求都可以找中国空翻协助\n1、 南迪→首都苏瓦：租车自驾\n南迪机场出来后拿上车直接出发（租车要提前预定），这条自驾线路性价比很高，沿着斐济最好的沿海环岛公路-皇后大道，一路开到底，特别有《后会无期》电影的感觉。道路笔直，车辆很少，你会看到几乎只有你一辆车在跑，如果租的是敞篷车肯定很赞，沿路是五颜六色村庄，苗条的椰子树，青蓝的海…驱车的过程本身就是一种享受，每到一个你觉得漂亮的地方就停留玩闹片刻，用身体的每个细胞去感受着一切。",
-                "img": [
-                    "upload/55b327c54b9a3.jpg",
-                    "upload/55b327ca21f6f.jpg"
-                ]
-            }
-        ]
-    },
-    {
-        "type": "simple",
-        "content": {
-            "txt": "塔韦乌尼岛（taveuni）：被称为“花园岛”，是一个火山岛。目前还幸免于商业采伐的这里是真正的荒！真正的原生态!真正的纯天然！浓密的原始雨林，馥郁的绿色棕榈，遍地的热带野花，各种鸟类，这美丽景致并非只在陆地，更是延伸至水下，恍若置身于世外桃源，纯洁美好！这里人烟稀少，是迎接第一缕阳光的地方，这里就是《爸爸去哪2》的拍摄地点，也是《珊瑚礁探险之旅》《重返蓝色珊瑚礁》拍摄地。",
-            "img": []
-        }
-    },
-    {
-        "type": "simple",
-        "content": {
-            "txt": "1、 彩虹礁潜水\n是世界第三大堡礁群，最著名的软珊瑚区，这里在潜水者们心中获得了神话般的地位，在索莫索莫海峡观赏美不胜收的珊瑚，五彩斑斓的热带鱼，还有章鱼、鲨鱼、海龟，甚至巨头鲸。这里最大的亮点有：发光的大白墙（great white wall），密布白色软珊瑚，看起来像摇摆发光的雪花。紫墙（purple wall）满是紫色软珊瑚，海扇海鞭，迷幻游离。从跳下水的那一刻，你只会exciting!",
-            "img": [
-                "upload/55b335d99cb35.jpg"
-            ]
-        }
-    },
-    {
-        "type": "simple",
-        "content": {
-            "txt": "2、 抵达→入住花园岛酒店\n机场到花园岛酒店大约20分钟（可以请求机场工作人员叫taxi或者提前跟酒店预订接机服务），花园岛酒店的诚信很好，电话预定，连押金任何东西都不用交，一切等你走的那天在结算。你不禁会开玩笑直接跑掉都可以！哈哈！入住后先提前预约一下第二天的彩虹礁水肺潜水。",
-            "img": []
-        }
-    }
-];
 
-function structcontent() {
-    this.$structContentWrap = $('.structContentWrap');
-    this.$structContainerContent = this.$structContentWrap.children('.struct-container-content');
-    this.$container = $(document.body);
+var structcontent;
+
+if (structcontent == undefined) {
+    structcontent = function (settings) {
+        this.init(settings);
+    };
 }
 
-structcontent.prototype.init = function () {
+/**
+ * 初始化操作+配置参数
+ */
+structcontent.prototype.init = function (settings) {
+
+    this.settings = settings;
+
+    this.initSettings();
+
     this.structCell();
 
+    this.loadFunc();
+};
+
+
+structcontent.prototype.initSettings = function () {
+    this.ensureDefault = function (settingName, defaultValue) {
+        this.settings[settingName] = (this.settings[settingName] == undefined) ? defaultValue : this.settings[settingName];
+    };
+
+    this.ensureDefault("structClass", "");
+    this.ensureDefault("structField", "");
+    this.ensureDefault("structData", "");
+
+    this.$structContentWrap = $('.' + this.settings.structClass);
+    this.$structContentWrap.append(template('Struct-Tool-Bar'));
+    this.$structContentWrap.append(template('T-Struct-Body'));
+
+    this.$structContainerContent = this.$structContentWrap.children('.struct-container-content');
+    this.$container = this.$structContentWrap;
+}
+
+/**
+ * 所有功能函数集合
+ */
+structcontent.prototype.loadFunc = function () {
     this.addSiblingCell();
 
     this.addSiblingCellSimple();
@@ -108,11 +61,12 @@ structcontent.prototype.init = function () {
 
     this.deleteCellContent();
 
+    //this.transferCell();
+
     this.uploadPic();
 
     this.saveAllCell();
-
-};
+}
 
 /**
  * 事件 集合
@@ -130,13 +84,14 @@ structcontent.prototype.eventCollection = function () {
  * 初始化结构单元
  */
 structcontent.prototype.structCell = function () {
-    var self = this;
-    //structData = '';
+    var self = this,
+        cellContent;
+
     if (structData) {
-        var cellContent = template('T-Struct-Display', {structData: structData});
+        cellContent = template('T-Struct-Display', {structData: self.settings.structData});
         self.$structContainerContent.append(cellContent);
     } else {
-        var cellContent = template('T-Advance', {});
+        cellContent = template('T-Advance', {});
         self.$structContainerContent.append(cellContent);
     }
 
@@ -219,7 +174,7 @@ structcontent.prototype.downCell = function () {
 
     self.$container.on("click", '.downCellBtn', function () {
         var currentParent = $(this).parents('.struct-cell');
-        var cellLength = $('.struct-container-content > .struct-cell').length;
+        var cellLength = self.$container.find('.struct-container-content > .struct-cell').length;
         if (currentParent.index() == (cellLength - 1)) {
             alert('好累，下移不了!');
             return;
@@ -231,6 +186,46 @@ structcontent.prototype.downCell = function () {
 
     });
 }
+
+/**
+ * 高级单元和简单单元之间切换
+ */
+structcontent.prototype.transferCell = function () {
+    var self = this;
+    self.$container.on('click', '.transferCellBtn', function () {
+        var structCellObj = $(this).parents('.struct-cell');
+        var structCellType = structCellObj.find('.struct-type').val();
+        var structContentLength = structCellObj.find('.cell-content > .cell-content-child').length;
+        if (structContentLength > 1) {
+            alert("内容太多，无法切换!");
+            return;
+        } else if (structContentLength == 1) {
+            var cellContentTxt = structCellObj.find('.text_val').val();
+            var cellContentImg = structCellObj.find('.cell-content-img').html();
+
+            if (structCellType == 'advance') {
+                var cellContentTitle = structCellObj.find('.title_val').val();
+                var simpleContent = template('T-Simple',{'cellContentTxt':cellContentTxt});
+                structCellObj.after(simpleContent);
+
+            } else if (structCellType == 'simple') {
+
+            } else {
+                alert("结构单元类型不存在！");
+                return;
+            }
+
+
+
+
+        } else {
+            alert("无法识别您的意思！");
+            return;
+        }
+    });
+}
+
+
 /**
  * 删除结构单元
  */
@@ -253,12 +248,20 @@ structcontent.prototype.deleteCell = function () {
 structcontent.prototype.deleteCellContent = function () {
     var self = this;
     self.$container.on("click", '.delCellContentBtn', function () {
-        if (confirm('确认删除吗？')) {
-            $(this).parents('.cell-content-child').remove();
+        var cellContentChildCount = $(this).parents('.cell-content').find('.cell-content-child').length;
+        if (cellContentChildCount > 1) {
+            if (confirm('确认删除吗？')) {
+                $(this).parents('.cell-content-child').remove();
 
-            self.eventCollection();
+                self.eventCollection();
+            }
+            return false;
+        } else {
+            alert('放过我吧！');
+            return false;
         }
-        return false;
+
+
     });
 }
 
@@ -279,7 +282,10 @@ structcontent.prototype.saveAllCell = function () {
 
 structcontent.prototype.refreshActionEvent = function () {
     // 所有结构单元 对象
-    var structCellObj = $('.struct-container-content > .struct-cell');
+    var self = this;
+
+    var structCellObj = self.$container.find('.struct-container-content > .struct-cell');
+
     var cellLength = structCellObj.length;
     if (cellLength <= 1) {
         //只有一个单元时候，因此 删除、上移和下移
@@ -288,23 +294,25 @@ structcontent.prototype.refreshActionEvent = function () {
         structCellObj.eq(0).find('.downCellBtn').hide();
     } else {
         //所有的操作都显示
-        $('.struct-cell .delCellBtn').show();
-        $('.struct-cell .upCellBtn').show();
-        $('.struct-cell .downCellBtn').show();
+        self.$container.find('.struct-cell .delCellBtn').show();
+        self.$container.find('.struct-cell .upCellBtn').show();
+        self.$container.find('.struct-cell .downCellBtn').show();
         //第一个没有上移
         structCellObj.eq(0).find('.upCellBtn').hide();
 
         //最后一个没有下移
         structCellObj.eq(cellLength - 1).find('.downCellBtn').hide();
     }
+
 }
 
 /**
  * 单元模型 存储到JSON中
  */
 structcontent.prototype.storeJson = function () {
+    var self = this;
     var structContent = [];
-    $('.struct-container-content > .struct-cell').each(function (index) {
+    self.$container.find('.struct-container-content > .struct-cell').each(function (index) {
         var structType = $(this).find('.struct-type').val();
         var arrContent = [];//内容数组
         if (structType == 'advance') {
@@ -354,10 +362,10 @@ structcontent.prototype.storeJson = function () {
     });
 
     var structJsonContent = JSON.stringify(structContent);
-    $('#struct_content').val(structJsonContent);
+    $("input[name='" + self.settings.structField + "']").val(structJsonContent);
 
     //用于右侧显示模块
-    $('#showJsonData').html(JSON.stringify(structContent, null, 2));
+    $('#' + self.settings.showStructDataField).html(JSON.stringify(structContent, null, 2));
 }
 
 /**
@@ -420,9 +428,3 @@ structcontent.prototype.uploadPic = function () {
         }
     });
 }
-
-
-var structcontent = new structcontent();
-$(function () {
-    structcontent.init();
-})
